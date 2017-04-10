562b2d47bd5b3c9c8fb94e262843051e74d2f47c
MilesZhao <mileszhao1990@yahoo.com>
2017-04-10 14:02:25 -0400

    Create the add to do item action respone

--- index.php
+++ index.php
@@ -16,9 +16,29 @@ if($action == "show_login_page")
   $suc = isUserValid($username,$password);
   if($suc == true)
   {
-    echo ' you are in right now!';    
+    $result = getTodoItems($_COOKIE['my_id']);
+    include('list.php');
   }else{
-    echo "wrong user name or password!";
+    header("Location: badInfo.php");
   }
+}else if ($action == 'registrar')
+{
+ // echo " we want to create a new account";
+  $name = filter_input(INPUT_POST, 'reg_uname');
+  if(isset($name))
+  {
+     $pass = filter_input(INPUT_POST, 'reg_password');
+     $exit = createUser($name,$pass);
+     if($exit == true)
+     {
+       include('user_exit.php');
+     }else {
+       header("Location: login.php");
+     }
+  }
+}else if ($action == 'add')
+{
+
+
 }
 ?>
